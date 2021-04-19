83ac6a35cd19067bd45f70c89dbec5736bf93997
JIAYINIU <55513791+JIAYINIU@users.noreply.github.com>
2021-04-19 12:42:40 +0800

    Add files via upload

---
+++ plot2.R
@@ -0,0 +1,62 @@
+## Downloads data
+Data <-
+  read.table(
+    "/Users/jiayiniu/Downloads/household_power_consumption.txt",
+    header = TRUE,
+    sep = ";",
+    na.strings = "?",
+    colClasses = c(
+      'character',
+      'character',
+      'numeric',
+      'numeric',
+      'numeric',
+      'numeric',
+      'numeric',
+      'numeric',
+      'numeric'
+    ),
+    col.names = c(
+      "Date",
+      "Time",
+      "Global_active_power",
+      "Global_reactive_power",
+      "Voltage",
+      "Global_intensity",
+      "Sub_metering_1",
+      "Sub_metering_2",
+      "Sub_metering_3"
+    ))
+
+## Tidy the data    
+# Reformat date to standard date form
+Data$Date <- as.Date(Data$Date, "%d/%m/%Y")
+
+# Filter data set from Feb.1 2007 to Feb.2 2007
+Data <-subset(Data, Date >= as.Date("2007-2-1") & Date <= as.Date("2007-2-2"))
+
+# Remove incomplete observations
+Data <- Data[complete.cases(Data), ]
+
+# Combine Date and Time column
+dateTime <- paste(Data$Date, Data$Time)
+
+# Rename the vector
+dateTime <- setNames(dateTime, "DateTime")
+
+# Drop the Date and Time columns
+Data <- Data[-c(1,2)]
+
+# Add DateTime column
+Data <- cbind(dateTime, Data)
+
+# Format dateTime Column
+Data$dateTime <- as.POSIXct(dateTime)
+
+## Plot 2
+# Plot the time series plot
+plot(Data$Global_active_power~Data$dateTime, type="l", ylab="Global Active Power (kilowatts)", xlab="")
+
+# Save file and close device
+dev.copy(png,"plot2.png", width=480, height=480)
+dev.off()
---
+++ plot4.R
@@ -0,0 +1,68 @@
+## Downloads data
+Data <-
+  read.table(
+    "/Users/jiayiniu/Downloads/household_power_consumption.txt",
+    header = TRUE,
+    sep = ";",
+    na.strings = "?",
+    colClasses = c(
+      'character',
+      'character',
+      'numeric',
+      'numeric',
+      'numeric',
+      'numeric',
+      'numeric',
+      'numeric',
+      'numeric'
+    ),
+    col.names = c(
+      "Date",
+      "Time",
+      "Global_active_power",
+      "Global_reactive_power",
+      "Voltage",
+      "Global_intensity",
+      "Sub_metering_1",
+      "Sub_metering_2",
+      "Sub_metering_3"
+    ))
+
+## Tidy the data    
+# Reformat date to standard date form
+Data$Date <- as.Date(Data$Date, "%d/%m/%Y")
+
+# Filter data set from Feb.1 2007 to Feb.2 2007
+Data <-subset(Data, Date >= as.Date("2007-2-1") & Date <= as.Date("2007-2-2"))
+
+# Remove incomplete observations
+Data <- Data[complete.cases(Data), ]
+
+# Combine Date and Time column
+dateTime <- paste(Data$Date, Data$Time)
+
+# Rename the vector
+dateTime <- setNames(dateTime, "DateTime")
+
+# Drop the Date and Time columns
+Data <- Data[-c(1,2)]
+
+# Add DateTime column
+Data <- cbind(dateTime, Data)
+
+# Format dateTime Column
+Data$dateTime <- as.POSIXct(dateTime)
+
+## Plot 4
+par(mfrow = c(2, 2))
+plot(Data$dateTime, Data$Global_active_power, xlab = '', ylab = 'Global Active Power (kilowatt)', type = 'l')
+plot(Data$dateTime, Data$Voltage, xlab = 'datetime', ylab = 'Voltage', type = 'l')
+plot(Data$dateTime, Data$Sub_metering_1, xlab = '', ylab = 'Energy sub metering', type = 'l')
+lines(Data$dateTime, Data$Sub_metering_2, col = 'red')
+lines(Data$dateTime, Data$Sub_metering_3, col = 'blue')
+legend('topright', col = c('black', 'red', 'blue'), legend = c('Sub_metering_1', 'Sub_metering_2', 'Sub_metering_3'), lwd = 1)
+plot(Data$dateTime, Data$Global_reactive_power, xlab = 'datetime', ylab = 'Global_reactive_power', type = 'l')
+
+# Save file and close device
+dev.copy(png,"plot4.png", width=480, height=480)
+dev.off()
